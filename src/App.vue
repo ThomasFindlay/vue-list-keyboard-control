4090da0e4fabf69945a3a7e4e45737c9c2f23ff3
Thomas Findlay <kotowicztom@gmail.com>
2018-07-02 18:17:31 +0100

    final

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <with-keyboard-control :listLength="$options.list.length" @selected="selectedHandler">
+      <template slot-scope="{selectedIndex}">
+        <div
+          v-for="(item, index) in $options.list"
+          class="list-item"
+          :class="{'selected': index === selectedIndex}"
+          :key="index">
+          <p>{{item}}</p>
+        </div>
+      </template>
+    </with-keyboard-control>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import WithKeyboardControl from "./components/WithKeyboardControl";
 export default {
   name: "app",
   components: {
-    HelloWorld
+    WithKeyboardControl
+  },
+  methods: {
+    selectedHandler(selectedIndex) {
+      alert(
+        `The item '${
+          this.$options.list[selectedIndex]
+        }' with index ${selectedIndex} has been selected!`
+      );
+    }
+  },
+  created() {
+    this.$options.list = ["hello", "world", "this", "is", "a", "list"];
   }
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+.list-item {
+  width: 150px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  border: 1px solid #999;
+  cursor: pointer;
+}
+
+.list-item:hover,
+.list-item.selected {
+  background-color: yellowgreen;
 }
 </style>
